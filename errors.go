eb9a1b3865a6f1d93e9fc19fc35ca5db0dce4715
Ryan Westlund <rlwestlund@gmail.com>
2020-02-13 19:48:57 +0000

    Return QBO errors properly

---
+++ errors.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2020, Randy Westlund. All rights reserved.
+// This code is under the BSD-2-Clause license.
+
+package quickbooks
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+// Error implements the error interface.
+func (f Failure) Error() string {
+	var text, err = json.Marshal(f)
+	if err != nil {
+		return "When marshalling error:" + err.Error()
+	}
+	return string(text)
+}
+
+// Failure is the outermost struct that holds an error response.
+type Failure struct {
+	Fault struct {
+		Error []struct {
+			Message string
+			Detail  string
+			Code    string `json:"code"`
+			Element string `json:"element"`
+		}
+		Type string `json:"type"`
+	}
+	Time Date `json:"time"`
+}
+
+// parseFailure takes a response reader and tries to parse a Failure.
+func parseFailure(res *http.Response) error {
+	var msg, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return errors.New("When reading response body:" + err.Error())
+	}
+	var errStruct Failure
+	err = json.Unmarshal(msg, &errStruct)
+	if err != nil {
+		return errors.New(strconv.Itoa(res.StatusCode) +
+			" " + string(msg))
+	}
+	return errStruct
+}
