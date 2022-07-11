3c7927540a8b8f3240fe2417a94ce3c836420aaf
Ken Little <ken@justworkslabs.com>
2022-07-11 17:35:25 -0400

    use our fork

--- examples/reuse_token_test.go
+++ examples/reuse_token_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/rwestlund/quickbooks-go"
+	"github.com/neonmoose/quickbooks-go"
 	"github.com/stretchr/testify/require"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/rwestlund/quickbooks-go
+module github.com/neonmoose/quickbooks-go
 
 go 1.18
 
